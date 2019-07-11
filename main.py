f834332dbb29e24995d347abbbe11d5a9d169620
RiceRing <1095352502@qq.com>
2019-07-11 18:17:26 +0800

    attack和defense组单独预测 准确率30-40

--- main.py
+++ main.py
@@ -36,3 +36,26 @@ print(y_predict)
 score = accuracy_score(y_test, y_predict)
 print(score)
 
+# 预测defence
+data = []
+label = []
+file = pd.read_csv("./data/DefenseDecision.csv")
+df = pd.DataFrame(file)
+for index in range(0,85):
+    if df.ix[index][0] in valid_num:
+        label.append(df.ix[index][1])
+        data.append(df.ix[index][2:-1])
+
+# 拆分训练数据和测试数据
+x = np.array(data)
+y = np.array(label)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+
+clf = neighbors.KNeighborsClassifier(algorithm="kd_tree")
+clf.fit(x_train, y_train)
+
+y_predict = clf.predict(x_test)
+print(y_test)
+print(y_predict)
+score = accuracy_score(y_test, y_predict)
+print(score)
