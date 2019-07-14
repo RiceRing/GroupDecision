40f4f2bd90cc35b88874907642951889b4de8a41
RiceRing <1095352502@qq.com>
2019-07-14 23:24:35 +0800

    合并knn分析

--- main.py
+++ main.py
@@ -9,30 +9,37 @@ file = pd.read_csv("./GroupData.csv")
 df = pd.DataFrame(file)
 
 # 获得完整数据的组号
-valid_num = []
-for index in range(0, 55):
-    valid_num.append(df.ix[index][0])
-
 data = []
-label = []
-file = pd.read_csv("./data/AttackDecision.csv")
-df = pd.DataFrame(file)
-for index in range(0,85):
-    if df.ix[index][0] in valid_num:
-        label.append(df.ix[index][1])
-        data.append(df.ix[index][2:-1])
+a_label = []
+d_label = []
+for index in range(0, 55):
+    data.append(df.ix[index][5:])
+    a_label.append(df.ix[index][3])
+    d_label.append(df.ix[index][2])
+
 
 # 拆分训练数据和测试数据
 x = np.array(data)
-y = np.array(label)
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+a = np.array(a_label)
+d = np.array(d_label)
+x_train, x_test, y_train, y_test = train_test_split(x, a, test_size=0.2)
 
 clf = neighbors.KNeighborsClassifier(algorithm="kd_tree")
 clf.fit(x_train, y_train)
 
 y_predict = clf.predict(x_test)
-print(y_test)
-print(y_predict)
+print("attack test：", y_test)
+print("attack predict:", y_predict)
 score = accuracy_score(y_test, y_predict)
-print(score)
+print("Test score:", score)
 
+x_train, x_test, y_train, y_test = train_test_split(x, d, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier(algorithm="kd_tree")
+clf.fit(x_train, y_train)
+
+y_predict = clf.predict(x_test)
+print("defense test：", y_test)
+print("defense predict:", y_predict)
+score = accuracy_score(y_test, y_predict)
+print("Test score:", score)
